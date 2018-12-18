541ad899d78c52c7357f034cbec9deecffac278a
Michal Nedzka <michael.nedzka@gmail.com>
2018-12-18 23:23:29 +0100

    Add setup for using pug templating engine

--- routes/admin.js
+++ routes/admin.js
@@ -5,13 +5,14 @@ const rootDir = require("../util/path");
 
 const router = express.Router();
 
+const products = [];
+
 router.get("/add-product", (req, res, next) => {
-  console.log("in the middleware");
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", { pageTitle: "add product" });
 });
 
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
