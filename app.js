a0226a0a9a17ba9ec0dc4ec04e29b3319e3a145a
Michal Nedzka <michael.nedzka@gmail.com>
2018-12-18 22:36:42 +0100

    Separate styles static function, create utils

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
